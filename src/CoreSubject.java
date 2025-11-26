7314f333cc9257b674261a5c05b47f68f1e5f6d3
kakwezi peace <peacekakwezi222@gmail.com>
2025-11-26 15:26:10 +0200

    CoreSubject extending super class Subject

--- src/CoreSubject.java
+++ src/CoreSubject.java
@@ -1,2 +1,20 @@
-public class CoreSubject {
+// CoreSubject.java
+public class CoreSubject extends Subject {
+    private final boolean mandatory = true;
+
+    public CoreSubject(String name, String code) {
+        super(name, code);
+    }
+
+    public boolean isMandatory() { return mandatory; }
+
+    @Override
+    public void displaySubjectDetails() {
+        System.out.println(getSubjectName() + " (" + getSubjectCode() + ") - Core, mandatory: " + mandatory);
+    }
+
+    @Override
+    public String getSubjectType() {
+        return "Core";
+    }
 }
