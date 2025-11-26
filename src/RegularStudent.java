f5ec2b9da81a6f8b75de97a43e5343a6847c9040
kakwezi peace <peacekakwezi222@gmail.com>
2025-11-26 12:19:54 +0200

    Declaring passingGrade variable ,inserting parameters inside the constructor  and overriding methods from the parent class

--- src/RegularStudent.java
+++ src/RegularStudent.java
@@ -1,2 +1,31 @@
-public class RegularStudent {
+// RegularStudent.java
+public class RegularStudent extends Student {
+    private final double passingGrade = 50.0;
+
+
+    // this is the constructor inheriting traits from the super class using Super key word.
+    public RegularStudent(String name, int age, String email, String phone) {
+        super(name, age, email, phone);
+    }
+
+      // these are the methods that i overriden from the parent class
+    @Override
+    public void displayStudentDetails() {// will display student info
+        System.out.println("ID: " + getStudentId());
+        System.out.println("Name: " + getName());
+        System.out.println("Type: Regular Student");
+        System.out.println("Passing Grade: " + passingGrade + "%");
+        System.out.println("Status: " + getStatus());
+    }
+
+    @Override
+    public String getStudentType() {
+        return "Regular";
+    }
+
+    @Override
+    public double getPassingGrade() {
+        return passingGrade;
+    }
 }
+
