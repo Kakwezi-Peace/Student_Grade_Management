69047e30c6df2782f3504ce2c4695e8a2dda4f7b
kakwezi peace <peacekakwezi222@gmail.com>
2025-11-26 14:11:12 +0200

    Overriding methods from superclass, displaying std info and constructor creation

--- src/HonorsStudent.java
+++ src/HonorsStudent.java
@@ -1,2 +1,36 @@
-public class HonorsStudent {
+// HonorsStudent.java
+public class HonorsStudent extends Student {
+    private static final double PASSING_GRADE = 60.0;
+    private boolean honorsEligible = false; // derived from average
+
+    public HonorsStudent(String name, int age, String email, String phone) {
+        super(name, age, email, phone);
+    }
+
+
+    public boolean isHonorsEligible() {
+        return honorsEligible;
+    }
+
+    @Override
+    public void displayStudentDetails() {
+        System.out.println("Student ID: " + getStudentId());
+        System.out.println("Name: " + getName());
+        System.out.println("Type: Honors");
+        System.out.println("Age: " + getAge());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Passing Grade: " + PASSING_GRADE + "%");
+        System.out.println("Honors Eligible: " + (honorsEligible ? "Yes" : "No"));
+        System.out.println("Status: " + getStatus());
+    }
+
+    @Override
+    public String getStudentType() {
+        return "Honors";
+    }
+
+    @Override
+    public double getPassingGrade() {
+        return PASSING_GRADE;
+    }
 }
