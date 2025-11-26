71ef79345a3c2145ffb33417971f69d91bbe009c
kakwezi peace <peacekakwezi222@gmail.com>
2025-11-26 15:17:08 +0200

    Subject abstract class creation with var,constructors, getters and setters  and methods

--- src/Subject.java
+++ src/Subject.java
@@ -1,2 +1,28 @@
-public class Subject {
+// Subject.java
+public abstract class Subject {
+    // variables
+    private String subjectName;
+    private String subjectCode;
+
+
+     // the constructor called from the parent class
+    public Subject(String subjectName, String subjectCode) {
+        this.subjectName = subjectName;
+        this.subjectCode = subjectCode;
+    }
+  // getters
+    public String getSubjectName()
+    { return subjectName; }
+    public String getSubjectCode()
+    { return subjectCode; }
+
+    // setters
+    public void setSubjectName(String subjectName)
+    { this.subjectName = subjectName; }
+    public void setSubjectCode(String subjectCode)
+    { this.subjectCode = subjectCode; }
+
+    // abstract methods from super class
+    public abstract void displaySubjectDetails();
+    public abstract String getSubjectType();
 }
