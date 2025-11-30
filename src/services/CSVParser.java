2b9c74144fdd1d9d1ef79c8168a78c703229d5f2
kakwezi peace <peacekakwezi222@gmail.com>
2025-12-01 01:20:15 +0200

    Creation of CSVParser  Class

--- src/services/CSVParser.java
+++ src/services/CSVParser.java
@@ -1,4 +1,9 @@
 package services;
 
-public class CSVParser {
+import exceptions.InvalidFileFormatException;
+
+public interface CSVParser {
+    // returns rows of [studentId, subjectName, subjectType, gradeString]
+    String[][] parse(String path) throws InvalidFileFormatException;
 }
+
