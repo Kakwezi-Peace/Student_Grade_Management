dd3f42b0e78016b157cf10c3187e9eafd693862b
kakwezi peace <peacekakwezi222@gmail.com>
2025-12-02 19:53:30 +0200

    Formed GPACalculator Class

--- src/services/CSVParser.java
+++
@@ -1,9 +0,0 @@
-package services;
-
-import exceptions.InvalidFileFormatException;
-
-public interface CSVParser {
-    // returns rows of [studentId, subjectName, subjectType, gradeString]
-    String[][] parse(String path) throws InvalidFileFormatException;
-}
-
