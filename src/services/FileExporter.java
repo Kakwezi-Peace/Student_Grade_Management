b656760dba5573efe1f2979e41aeade0c7dff357
kakwezi peace <peacekakwezi222@gmail.com>
2025-12-05 19:04:47 +0200

    feat(gpa): added a hunked part of FileExporter

--- src/services/FileExporter.java
+++ src/services/FileExporter.java
@@ -8,4 +8,12 @@ import java.io.IOException;
 
 
 public class FileExporter {
+    // SRP: Only handles file operations
+    public void exportToFile(String filename, String content) throws ExportFailedException, Throwable {
+        try (FileWriter writer = new FileWriter("./reports/" + filename + ".txt")) {
+            writer.write(content);
+        } catch (IOException e) {
+            throw new Throwable("Failed to export report: " + e.getMessage());
+        }
+    }
 }
