4d7a7d3bba7deff7e7be1a761e50f4ce6afca99c
kakwezi peace <peacekakwezi222@gmail.com>
2025-12-05 18:36:25 +0200

    feat(gpa): add InvalidGrade Exception

--- src/exceptions/InvalidGradeException.java
+++ src/exceptions/InvalidGradeException.java
@@ -1,7 +1,8 @@
 package exceptions;
 
+// Custom checked exception for invalid grade values
 public class InvalidGradeException extends Exception {
-    public InvalidGradeException(double grade) {
+    public InvalidGradeException(int grade) {
         super("Grade must be between 0 and 100. You entered: " + grade);
     }
 }
